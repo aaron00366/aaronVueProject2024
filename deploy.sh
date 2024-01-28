63628ef12f3cafe3fd54e22d1dd268ca5cffd80a
aaron00366 <65890513+aaron00366@users.noreply.github.com>
2024-01-28 22:03:03 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,16 @@ cd dist
 # 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
 # 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:aaron00366/aaronVueProject2024.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f https://aaron00366.github.io/aaronVueProject2024 master:gh-pages
+# git push -f https://aaron00366.github.io/aaronVueProject2024.git main:gh-pages
 # git push -f https://github.com/aaron00366/aaronVueProject2024.git master:gh-pages
 
 cd -
--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base: '/live-vue3-dashboard-vite/',
-  base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  base: 'aaronVueProject2024/',
+  // base: process.env.NODE_ENV === 'production' ? `/${process.env.REPOSITORY_NAME}/` : '/',
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+        output: {
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+},
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
